ac449a3e14546f3eb3ec839960df1f823bc58943
Braden Haskell <braden.haskell@outlook.com>
2018-01-31 03:56:20 +0000

    Clean up the submenu just a bit

--- index.js
+++ index.js
@@ -2,17 +2,24 @@
 document.onreadystatechange = function() {
     if (document.readyState == 'interactive') {
         
+        // hide menu if clicked anywhere
+        document.addEventListener("click", function(e) {
+            var divTag = e.target.parentNode;
+            var classes = divTag.classList;
+            if (!(classes.contains("settings-menu") || classes.contains("settings-menu-options") || classes.contains("menu-title") || classes.contains("menu-items") || classes.contains("settings-button"))) {
+                document.getElementById("settings-menu").style.display = "none";
+            }
+        });
+        
         // get the settings button
         var settingsBtn = document.getElementsByClassName("settings-button")[0];
-        var settingsMenuShow = 0;
+        var settingsMenu = document.getElementById("settings-menu");
         settingsBtn.addEventListener("click", function() {
-            if (settingsMenuShow == 0) {
-                document.getElementById("settings-menu").style.display = "inline-block";
-                settingsMenuShow = 1;
+            if (settingsMenu.style.display == "none") {
+                settingsMenu.style.display = "inline-block";
             }
             else {
-                document.getElementById("settings-menu").style.display = "none";
-                settingsMenuShow = 0;
+                settingsMenu.style.display = "none";
             }
         });
     }
